d0996c20e2a0394853f162d66f31be492b4b5c8c
Dmitry Verteyko <verteyko1990@gmail.com>
2020-04-02 22:52:36 +0300

    unit testing

--- frontend/tests/unit/header.spec.js
+++ frontend/tests/unit/header.spec.js
@@ -1,12 +1,20 @@
-// import { shallowMount } from '@vue/test-utils';
-// import HelloWorld from '@/components/HelloWorld.vue';
-//
-// describe('HelloWorld.vue', () => {
-//   it('renders props.msg when passed', () => {
-//     const msg = 'new message';
-//     const wrapper = shallowMount(HelloWorld, {
-//       propsData: { msg },
-//     });
-//     expect(wrapper.text()).toMatch(msg);
+import { shallowMount } from '@vue/test-utils';
+import HeaderTemplate from '../../src/components/HeaderTemplate.vue';
+
+describe('HeaderTemplate.vue', () => {
+  it('renders props.msg when passed', () => {
+    const msg = 'new message';
+    const wrapper = shallowMount(HeaderTemplate, {
+      propsData: { msg },
+    });
+    expect(wrapper.text()).toMatch(msg);
+  });
+});
+
+
+// describe('Content.vue', () => {
+//   test('render component Visibility', () => {
+//     const wrapper = shallowMount(Visibility);
+//     expect(wrapper.text()).toContain('Air Visibility'); (проверяет содержание данного текста в компоненте)
 //   });
 // });
